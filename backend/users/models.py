c73b663eac271c2647de2c080d133a42a86045c6
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2026-08-13 16:27:30 -0400

    created model user

---
+++ backend/users/models.py
@@ -0,0 +1,8 @@
+# LeoneLibrary/users/models.py
+from django.db import models
+
+class User(models.Model):
+    email: models.EmailField()
+    username: models.CharField(max_length=15)
+    password: models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
