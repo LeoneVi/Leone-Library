b803f0b01774588d4367dd820d1116a62b3b089c
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2026-09-02 16:01:50 -0400

    remove files I don't need from vue startup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   server: {
     proxy: {
       '/_allauth': 'http://127.0.0.1:8000',
